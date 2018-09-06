358b20705e5513d26627fe09752ac9a3f9c1ded1
dgorthi <deepthigorthi@berkeley.edu>
2018-09-06 18:04:39 +0200

    add millisec timestamp to each block in the hdf5 file

--- src/hera_hdf5_header.h
+++ src/hera_hdf5_header.h
@@ -5,19 +5,47 @@
 #include <hdf5.h>
 #include <time.h>
 
-#define N_BLOCK_PER_FILE 4096
+#define N_BLOCK_PER_FILE 32
 #define N_TIME_PER_FILE  (N_BLOCK_PER_FILE * N_TIME_PER_BLOCK)
 
-#define RANK             4
+/* The strategy for writing hdf5 files here is to build the entire file first
+   and then replace a chunk of the file as each block arrives and ready to 
+   be written. This is easy because once the size of the file is fixed, 
+   the number of blocks are set and the dataset does not need to be dynamically
+   expanded.
+*/
+
+#define FILE_DATA_RANK   4
+#define MEM_DATA_RANK    1
+
 #define DIM0             N_ANTS
-#define DIM1             2     //pols
+#define DIM1             2                   //pols
 #define DIM2             N_STRP_CHANS_PER_X
 #define DIM3             N_TIME_PER_FILE
 
-#define DIM0_SUB         N_ANTS
-#define DIM1_SUB         2
-#define DIM2_SUB         N_STRP_CHANS_PER_X
-#define DIM3_SUB         N_TIME_PER_BLOCK   
+#define DIM0_BLK         N_ANTS
+#define DIM1_BLK         2
+#define DIM2_BLK         N_STRP_CHANS_PER_X
+#define DIM3_BLK         N_TIME_PER_BLOCK
+
+#define MEM_DIM          (DIM0*DIM1*DIM2*DIM3)
+#define TIME_DIM         N_BLOCK_PER_FILE
+#define FILE_DIM         DIM0, DIM1, DIM2, DIM3
+#define BLOCK_DIM        (DIM0_BLK* DIM1_BLK* DIM2_BLK* DIM3_BLK)
+
+/* Parameters for hyperslab-bing the hdf5 file */
+/* Data */
+#define DCNT              1, 1, 1, 1         // Count- number of blocks in each dimension
+#define DSTD              1, 1, 1, 1         // Stride- number of points to skip
+// Block - unit of data in each dimension
+#define DBLK              N_ANTS, \
+                          2,      \
+                          N_STRP_CHANS_PER_X, \
+                          N_TIME_PER_BLOCK
+/* Time */
+#define TCNT              1
+#define TSTD              1
+#define TBLK              1               
 
 #define H5_WRITE_HEADER_I64(group_id, h5_name, val, dims) \
   dataspace_id = H5Screate_simple(1, dims, NULL); \
@@ -26,7 +54,6 @@
   H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &(val)); \
   H5Sclose(dataspace_id); \
   H5Dclose(dataset_id)
-
 
 typedef struct hdf5_extra_keywords{
     uint64_t kwargs;
@@ -42,7 +69,7 @@ typedef struct hdf5_header{
    int64_t Ntimes;              // Number of time samples
    int64_t ant_array[N_ANTS];    // Order of antenna numbers in data
    double channel_width;
-   double *unix_time_array;
+   char time_units[64];
 
 }hdf5_header_t;
 
--- src/paper_write_thread.c
+++ src/paper_write_thread.c
@@ -22,6 +22,8 @@ struct hdf5_header *initialize_header(){
    header->Nfreqs = N_STRP_CHANS_PER_X;
    header->channel_width = 250.0/8192.0;
    header->Ntimes = 131072;  // 32 per block* 4096 blocks
+   //header->time_units = (char *)malloc(128, sizeof(char));
+   strcpy(header->time_units, "millisec");
 
    for(i=0; i<N_ANTS; i++)
       header->ant_array[i] = i;
@@ -42,6 +44,7 @@ void write_hdf5_header(hdf5_header_t *header, hid_t file_id){
    H5_WRITE_HEADER_I64(group_id, "Ntimes", header->Ntimes, dims);
    H5_WRITE_HEADER_I64(group_id, "Npols", header->Npols, dims);
    H5_WRITE_HEADER_I64(group_id, "chan_width", header->channel_width, dims);
+   H5_WRITE_HEADER_I64(group_id, "time_units", header->time_units, dims);
    H5Gclose(group_id);
 }
 
@@ -57,25 +60,40 @@ void *paper_write_thread_run(hashpipe_thread_args_t *args){
     int block_id;
     uint64_t nblks = N_BLOCK_PER_FILE;
     char filename[4096];
+    struct timeval tv;
+    uint64_t now;
 
-    hid_t file_id, dataset_id, ds_file_id, ds_mem_id, ds_block_id;
-    hsize_t mem_dims[1] = {(DIM0*DIM1*DIM2*DIM3)};
-    
-    hsize_t dims[4] = {DIM0, DIM1, DIM2, DIM3};
-    hsize_t dimsm[1] = {(DIM0_SUB*DIM1_SUB*DIM2_SUB*DIM3_SUB)};
+    /* Datasets */
+    hid_t h5file, h5data, h5time;
+    /* Dataspaces */
+    hid_t h5ds_data_file, h5ds_data_mem, h5ds_data_block, 
+          h5ds_time, h5ds_time_entry;
+    /* Dimensions */
+    hsize_t mem_dim[] = {MEM_DIM};    hsize_t file_dim[] = {FILE_DIM};
+    hsize_t time_dim[] = {TIME_DIM};  hsize_t block_dim[] = {BLOCK_DIM};
+    hsize_t time_entry_dim[] = {1};
+
+    /* The strategy for writing hdf5 files here is to build the entire file first
+       and then replace a chunk of the file as each block arrives and ready to 
+       be written. This is easy because once the size of the file is fixed, 
+       the number of blocks are set and the dataset does not need to be dynamically
+       expanded.
+    */
+
+    hsize_t dcnt[] = {DCNT};   // Number of blocks in each dimension
+    hsize_t dstd[] = {DSTD};   // Don't skip any points 
+    hsize_t dblk[] = {DBLK};   // Define block
+ 
+    hsize_t tcnt[] = {TCNT};
+    hsize_t tstd[] = {TSTD};
+    hsize_t tblk[] = {TBLK};
     herr_t status;
-    hsize_t count[] = {1,1,1,1};
-    hsize_t stride[] = {1,1,1,1};
-    hsize_t block[] = {N_ANTS, 2, N_STRP_CHANS_PER_X, N_TIME_PER_BLOCK};
+
     uint8_t *init_data;
+    init_data = (uint8_t *)calloc(MEM_DIM,sizeof(uint8_t));
 
-    init_data = (uint8_t *)calloc(DIM0*DIM1*DIM2*DIM3,sizeof(uint8_t));
-    ds_mem_id = H5Screate_simple(1, mem_dims, NULL); 
-    printf("Bytes per block: %d\n", N_BYTES_PER_STRP_BLOCK);
-    printf("Blocks per file: %d\n", N_BLOCK_PER_FILE);
-    printf("Bytes per file: %lu\n", sizeof(init_data));
-
-    ds_file_id = H5Screate_simple(RANK, dims, NULL);
+    uint64_t *times;
+    times = (uint64_t *)calloc(N_TIME_PER_FILE, sizeof(uint64_t));
 
     while (run_threads()){
        
@@ -91,23 +109,34 @@ void *paper_write_thread_run(hashpipe_thread_args_t *args){
 
           /*Create a hdf5 file, set header, initialize a dataset and wait for data*/
           sprintf(filename, "hera_volt_data_%lu.h5", (unsigned long)time(NULL));
-          file_id = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-
+          printf("New file: %s\n\n",filename);
+          hid_t h5file = H5Fcreate(filename, H5F_ACC_TRUNC, 
+                                   H5P_DEFAULT, H5P_DEFAULT);
           hdf5_header_t *header = initialize_header();
-          write_hdf5_header(header, file_id);
+          write_hdf5_header(header, h5file);
 
-          dataset_id = H5Dcreate(file_id, "data", H5T_STD_U8BE, ds_file_id,
-                                 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+          h5ds_time      = H5Screate_simple(1,    time_dim, NULL);
+          h5ds_data_mem  = H5Screate_simple(MEM_DATA_RANK,  mem_dim,  NULL); 
+          h5ds_data_file = H5Screate_simple(FILE_DATA_RANK, file_dim, NULL);
 
-          status = H5Dwrite(dataset_id, H5T_NATIVE_UINT8, ds_mem_id, ds_file_id,
+          h5data = H5Dcreate(h5file, "data", H5T_STD_U8BE, h5ds_data_file,
+                             H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+          status = H5Dwrite(h5data, H5T_NATIVE_UINT8, h5ds_data_mem, h5ds_data_file,
                             H5P_DEFAULT, init_data);
+
+          h5time = H5Dcreate(h5file, "time", H5T_STD_U64BE, h5ds_time,
+                             H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+          status = H5Dwrite(h5time, H5T_NATIVE_UINT64, H5S_ALL, H5S_ALL, 
+                            H5P_DEFAULT, times);
 
           printf("Wrote init dataset to file\n");
 
-          status = H5Sclose(ds_file_id);
-          status = H5Sclose(ds_mem_id);
-          status = H5Dclose(dataset_id);
-          status = H5Fclose(file_id);      
+          status = H5Sclose(h5ds_data_file);
+          status = H5Sclose(h5ds_data_mem);
+          status = H5Sclose(h5ds_time);
+          status = H5Dclose(h5data);
+          status = H5Dclose(h5time);
+          status = H5Fclose(h5file);      
           status = status;
 
           nblks = 0;
@@ -127,32 +156,48 @@ void *paper_write_thread_run(hashpipe_thread_args_t *args){
           }
        }
 
-      /*Reopen hdf5 file with filename and rewrite the received block of data.*/
-      file_id = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
-      dataset_id = H5Dopen(file_id, "data", H5P_DEFAULT);
+      /*Reopen hdf5 file and write the received block of data.*/
+      h5file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+      h5data = H5Dopen(h5file, "data", H5P_DEFAULT);
+      h5time = H5Dopen(h5file, "time", H5P_DEFAULT);
 
-      hsize_t offset[4] = {0, 0, 0, nblks*N_TIME_PER_BLOCK};
-      ds_block_id = H5Screate_simple(1, dimsm, NULL);
+      hsize_t doffset[4] = {0, 0, 0, nblks*N_TIME_PER_BLOCK};
+      hsize_t toffset[] = {nblks};
+      h5ds_data_block = H5Screate_simple(1, block_dim, NULL);
+      h5ds_time_entry = H5Screate_simple(1, time_entry_dim, NULL);
 
-      ds_file_id = H5Dget_space(dataset_id);
-      status = H5Sselect_hyperslab(ds_file_id, H5S_SELECT_SET, offset,
-                                   stride, count, block);
+      h5ds_data_file = H5Dget_space(h5data);
+      status = H5Sselect_hyperslab(h5ds_data_file, H5S_SELECT_SET, 
+                                   doffset, dstd, dcnt, dblk);
+
+      h5ds_time = H5Dget_space(h5time);
+      status = H5Sselect_hyperslab(h5ds_time, H5S_SELECT_SET, 
+                                   toffset, tstd, tcnt, tblk);
 
       /*Copy data over*/
       data = (uint64_t *)idb->block[block_id].data;
-      status = H5Dwrite(dataset_id, H5T_NATIVE_UINT8, ds_block_id, ds_file_id, 
+      status = H5Dwrite(h5data, H5T_NATIVE_UINT8, h5ds_data_block, h5ds_data_file, 
                         H5P_DEFAULT, data);
 
-      status = H5Sclose(ds_file_id);
-      status = H5Sclose(ds_block_id);
-      status = H5Dclose(dataset_id);
-      status = H5Fclose(file_id);
+      gettimeofday(&tv, NULL);
+      now = (uint64_t)(tv.tv_sec*1000) + (uint64_t)(tv.tv_usec/1000);
+      status = H5Dwrite(h5time, H5T_NATIVE_UINT64, h5ds_time_entry, h5ds_time, 
+                        H5P_DEFAULT, &now);
+
+      status = H5Sclose(h5ds_data_file);
+      status = H5Sclose(h5ds_data_block);
+      status = H5Sclose(h5ds_time_entry);
+      status = H5Sclose(h5ds_time);
+      status = H5Dclose(h5data);
+      status = H5Dclose(h5time);
+      status = H5Fclose(h5file);
 
       // Mark input block as free, output block as filled
       paper_stripper_databuf_set_free(idb, block_id);
 
       // Setup for next block
       block_id = (block_id + 1)%idb->header.n_block;
+      printf("Number of blocks: %ld\n",nblks);
       nblks++;
 
       /* Will exit if thread has been cancelled */
